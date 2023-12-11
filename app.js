6ebb0d1e6f6d14e42253f330da5e027b201aea5a
JamesLi <lijames9532@gmail.com>
2023-12-11 16:47:05 -0500

    all

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const app = express();
+
+//process.env.PORT is for deployment purposes (yt tut deploys to Heroku)
+const PORT = process.env.PORT || 5000;
+
+//set template engine (ejs)
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//parse html data for POST request
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.post("/convert-mp3", async(req, res) => {
+    //grab name we game the input in the ejs file, that is why we need it
+    const videoId = req.body.videoID;
+    if(
+        videoId === undefined ||
+        videoId === "" ||
+        videoId === null
+    ) {
+        return res.render("index", {success : false, message: "Please enter a video ID"})
+    } else{
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+            "method" : "GET",
+            "headers" : {
+                "x-rapidapi-key" : process.env.API_KEY,
+                "x-rapidapi-host" : process.env.API_HOST
+            }
+        });
+
+        const fetchResponse = await fetchAPI.json();
+
+        //send these back to ejs file, where it is displayed
+        if(fetchResponse.status === "ok")
+            return res.render("index", {success: true, song_title:fetchResponse.title, song_link : fetchResponse.link});
+        else{
+            return res.render("index", {success: false, message : fetchResponse.msg});
+        }
+    }
+})
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
